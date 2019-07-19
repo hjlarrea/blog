509ffc58017e76b8d97e873050a6015b06dd7d16
Hernan J. Larrea <hjlarrea@hotmail.com>
2019-07-19 18:24:37 -0300

    Updated README file.

--- Build-Artifact.ps1
+++ Build-Artifact.ps1
@@ -1,6 +1,6 @@
 $ArticlesToPublish = Get-Content .\posts\posts.json | ConvertFrom-Json
 
-if(Test-Path .\publish) { Remove-Item -Recurse -Force }
+if(Test-Path -Path .\publish) { Remove-Item -Path .\publish -Recurse -Force }
 New-Item -Name publish -ItemType Directory
 New-Item -Path .\publish -Name "posts" -ItemType Directory
 
@@ -9,3 +9,7 @@ Copy-Item -Path ".\Format-Images.ps1" -Destination ".\publish" -Force
 Copy-Item -Path ".\Publish-Article.ps1" -Destination ".\publish" -Force
 
 $ArticlesToPublish | Where-Object { $_.posts.status -eq "publish"} | ConvertTo-Json -Depth 3 | Out-File .\publish\posts\posts.json
+
+($ArticlesToPublish | Where-Object { $_.posts.status -eq "publish"}).posts.folder | ForEach-Object {
+    Copy-Item -Path .\posts\$_ -Destination .\publish\posts -Recurse
+}
