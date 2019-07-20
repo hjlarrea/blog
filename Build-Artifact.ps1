a6be2fe7dc766789006a3d38de168ab1110b3ac7
Hernan J. Larrea <hjlarrea@hotmail.com>
2019-07-19 23:02:43 -0300

    Updated tooling.

--- Build-Artifact.ps1
+++ Build-Artifact.ps1
@@ -1,5 +1,9 @@
 $ArticlesToPublish = Get-Content .\posts\posts.json | ConvertFrom-Json
 
+if(($ArticlesToPublish | Where-Object { $_.posts.action -eq "publish"}).posts.Count -eq 0 ) {
+    throw "No articles to publish"
+}
+
 if(Test-Path -Path .\publish) { Remove-Item -Path .\publish -Recurse -Force }
 New-Item -Name publish -ItemType Directory
 New-Item -Path .\publish -Name "posts" -ItemType Directory
