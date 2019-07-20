d6a3e23b45003e6efe76c0cade6558883ba4e9cd
Hernan J. Larrea <hjlarrea@hotmail.com>
2019-07-19 21:53:30 -0300

    Updated tooling.

--- PublishTo-Blog.ps1
+++ PublishTo-Blog.ps1
@@ -9,7 +9,12 @@ $ArticlesToPublish = Get-Content .\posts\posts.json | ConvertFrom-Json
 foreach($ArticleToPublish in $ArticlesToPublish.posts) {
     $path = ".\posts\"+$ArticleToPublish.folder+"\post.md"
 
+    Write-Output "Transforming and uploading Gists..."
     .\Format-Gists.ps1 -Path $path -GitHubPAT $GitHubPAT -PostName $ArticleToPublish.name
+
+    Write-Output "Transforming links to images..."
     .\Format-Images.ps1 -Path $path
+
+    Write-Output "Publishing to Medium..."
     .\Publish-Article.ps1 -Title $ArticleToPublish.name -Path $path -Tags $ArticleToPublish.tags -userId $MediumUserId -Token $MediumToken -PublishStatus $ArticleToPublish.status 
 }
